c9ed0e4c5f6d334da598df505cf36994cba8c047
mathan31 <mathan.chandrasekar@gmail.com>
2021-07-01 06:10:29 +0530

    Python File Updated

--- Credo.py
+++ Credo.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
 application = Flask(__name__)
-app = application
 
-@app.route("/")
+@application.route("/")
 def index():
     return "<h1>WelCome to Credo World, Deploying in AWS EB!!!</h1>"
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
